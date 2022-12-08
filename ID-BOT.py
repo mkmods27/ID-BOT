5d30ec75d92f842494c5b50fa7215e6f91fe0ab5
DX-MODS <ziyankp44@gmail.com>
2022-12-08 22:09:41 +0530

    DX-MODS🔥

---
+++ ID-BOT.py
@@ -0,0 +1,56 @@
+##  ©2022 ZIYAN-SER
+from pyrogram import Client, filters
+from pyrogram.types import Message
+import os
+
+IdBotByZiyan=Client(
+    "IdBotByZiyan",
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"],
+    bot_token = os.environ["BOT_TOKEN"]
+)
+
+@IdBotByZiyan.on_message(filters.command('start') & filters.private)
+async def start(client, message):
+    text = f"""
+Heya {message.from_user.mention},
+I am here to provide Telegram user and chat ID.
+
+To get ID, simply send the command <code>/id</code> directly to any chat or send it as a reply to any message.
+
+I can also work in group chats.
+
+🏠 | [Home](https://t.me/suport_ziyans)
+            """
+    await IdBotByZiyan.send_message(message.chat.id, text, disable_web_page_preview=True)
+
+
+@IdBotByZiyan.on_message(filters.command('id'))
+async def get_id(client, message):
+    try:
+
+        if (not message.reply_to_message) and (message.chat):
+            await message.reply(f"User {message.from_user.first_name}'s ID is <code>{message.from_user.id }</code>.\nThis chat's ID is: <code>{message.chat.id}</code>.") 
+
+        elif not message.reply_to_message:
+            await message.reply(f"User {message.from_user.first_name}'s ID is <code>{message.from_user.id }</code>.") 
+
+        elif message.reply_to_message.forward_from_chat:
+            await message.reply(f"The forwarded {str(message.reply_to_message.forward_from_chat.type)[9:].lower()}, {message.reply_to_message.forward_from_chat.title} has an ID of <code>{message.reply_to_message.forward_from_chat.id}</code>.") 
+
+        elif message.reply_to_message.forward_from:
+            await message.reply(f"The forwarded user, {message.reply_to_message.forward_from.first_name} has an ID of <code>{message.reply_to_message.forward_from.id   }</code>.")
+
+        elif message.reply_to_message.forward_sender_name:
+            await message.reply("Sorry, you cannot get the forwarded user ID because of their privacy settings")
+
+        else:
+            await message.reply(f"User {message.reply_to_message.from_user.first_name}'s ID is <code>{message.reply_to_message.from_user.id}</code>.")   
+
+    except Exception:
+            await message.reply("An error occured while getting the ID.")
+
+print("IdBotByZiyan is alive!")
+IdBotByZiyan.run()
+
+
---
+++ README.md
@@ -0,0 +1,44 @@
+<h1 align= center>ID-BOT BY ZIYAN</h1>
+<h3 align = center>Fast and simple Telegram bot to provide user/group/channel ID. This bot can also work in group chats.</h3>
+<p align="center">
+<a href="https://python.org"><img src="http://forthebadge.com/images/badges/made-with-python.svg" alt="made-with-python"></a>
+<br>
+    <img src="https://img.shields.io/github/stars/ZIYAN-SER/ID-BOT?style=for-the-badge&color=yellow" alt="Stars">
+    <img src="https://img.shields.io/github/forks/ZIYAN-SER/ID-BOT?style=for-the-badge&color=green" alt="Forks">
+    <img src="https://img.shields.io/github/watchers/ZIYAN-SER/ID-BOT?style=for-the-badge&color=yellow" alt="Watchers"> <br>
+    <img src="https://img.shields.io/github/license/ZIYAN-SER/ID-BOT?style=for-the-badge&color=green" alt="License">
+    <img src="https://img.shields.io/github/repo-size/ZIYAN-SER/ID-BOT?style=for-the-badge&color=yellow" alt="Repository Size">
+    <img src="https://img.shields.io/github/contributors/ZIYAN-SER/ID-BOT?style=for-the-badge&color=green" alt="Contributors">
+    <img src="https://img.shields.io/github/issues/ZIYAN-SER/ID-BOT?style=for-the-badge&color=yellow" alt="Issues">
+</p>  
+
+## Deployment Methods
+
+### Heroku
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/ZIYAN-SER/ID-BOT)
+
+## Config Vars
+1. `API_ID` : Telegram API_ID, get it from my.telegram.org/apps
+2. `API_HASH` : Telegram API_ID, get it from my.telegram.org/apps
+3. `BOT_TOKEN` : A Valid Telegram Bot Token, get it from @Botfather
+
+## Commands
+
+  `/id`
+
+- Works in both private and group chat.
+```
+To get an ID, simply send the command /id directly to any chat or send it as a reply to any kind of message.
+```
+## Credits
+- [ID-BOT](https://github.com/ZIYAN-SER)
+- [Pyrogram](https://github.com/pyrogram/pyrogram)
+
+## Warning
+
+- You are free to use this code in any of your projects, but you MUST include the following in your README.md (Copy & paste)
+```
+##Credits
+- [ID-BOT BY ZIYAN] (https://github.com/ZIYAN-SER/ID-BOT)
+```
